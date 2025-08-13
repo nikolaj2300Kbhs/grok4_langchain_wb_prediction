13c8e96cccf400631754d102a4eccab554158b21
nikolaj2300Kbhs <nikolaj@bygoodiebox.com>
2025-08-13 16:53:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import os
 import re
 import logging
 import time
-from xai_sdk import Client
-from xai_sdk.chat import user
+from xai_grok_sdk import Client
+from xai_grok_sdk.chat import user
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
